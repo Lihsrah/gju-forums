1c3574b9a444ea256af020edf7fe540435f38356
harshil <harshilgodara@gmail.com>
2021-12-22 16:45:05 +0000

    stopped tracking .env file

--- backend/students/student.js
+++ backend/students/student.js
@@ -9,7 +9,7 @@ const { createToken,
 
 require('dotenv').config()
 const neo4j = require('neo4j-driver')
-// 
+
 const driver = neo4j.driver(process.env.URI, neo4j.auth.basic('neo4j', process.env.KEY), { disableLosslessIntegers: true }
 )
 const session = driver.session()
